7ac76c0b92fc884ba04d73e90e1d88ce437d7299
Robert Rodgers <rbrandon@zemation.com>
2018-12-10 16:39:56 -0700

    Updated Search Result Limit to 50

--- thewiz.py
+++ thewiz.py
@@ -7,12 +7,12 @@ import requests
 # Basic example GET https://api-endpoint.igdb.com/games/
 
 def get_games(games):
-    data = requests.get('https://api-endpoint.igdb.com/games/?search={}&fields=*'.format(games), headers=HEADERS).json()
+    data = requests.get('https://api-endpoint.igdb.com/games/?search={}&fields=*&limit=50'.format(games), headers=HEADERS).json()
     for i in data:
         print(i['name'])
 
 def get_characters(games):
-    data = requests.get('https://api-endpoint.igdb.com/characters/?search={}&fields=*'.format(games), headers=HEADERS).json()
+    data = requests.get('https://api-endpoint.igdb.com/characters/?search={}&fields=*&limit=50'.format(games), headers=HEADERS).json()
     for i in data:
         print(i['name'])
 
